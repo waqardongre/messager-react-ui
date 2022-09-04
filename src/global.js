f03b4a90c19d2cb87468fc11638ce88c8e92674c
waqardongre <waqardongre@gmail.com>
2022-09-05 04:13:28 +0530

    removed localhost from apiURL  which was changed to develop last functionality and updated it with live one

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
-//export const apiURL = "https://messager-node-api.vercel.app"
-export const apiURL = "http://localhost:5000"
+export const apiURL = "https://messager-node-api.vercel.app"
+//export const apiURL = "http://localhost:5000"
 
 //export const rootRoute = "/messager-react-ui" // for github pages
 export const rootRoute = "" // for others
